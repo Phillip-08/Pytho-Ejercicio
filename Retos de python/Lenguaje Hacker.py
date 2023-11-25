4fe4dd6f7afd5d956f62d8e086ea8ddb1b576bfd
Pipe <c.phillip.07@gmail.com>
2023-11-25 13:51:58 -0300

    Lenguaje Hacker

--- Retos de python/Lenguaje Hacker.py
+++ Retos de python/Lenguaje Hacker.py
@@ -1,2 +1,37 @@
-### Lenguaje Hacker ###
+### Lenguajes Hacker ###
 
+def traductor_leet ():
+    
+    dictionary = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1",
+                "J": ",_|", "K": ">|", "L": "1", "M": "/\/\\", "N": " ^/", "O": "0", "P": " |*", "Q": "(_,)",
+                "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/", "W": "\/\/", "X": "><", "Y": "j", "Z": "2",
+                "1": "L", "2": "R", "3": "E", "4": "A", "5": "S", "6": "b", "7": "T", "8": "B", "9": "g", "0": "o"}
+    
+    for i in dictionary():
+        print(i)
+        if i == "A":
+            print (dictionary.values)  
+        elif i == "B":
+            print (dictionary.values)
+        elif i == "C":
+            print (dictionary.values)
+        elif i == "D":
+            print (dictionary.values)
+        elif i == "E":
+            print (dictionary.values)
+        elif i == "F":
+            print (dictionary.values)
+        elif i == "G":
+            print (dictionary.values)
+        elif i == "H":
+            print (dictionary.values)
+        elif i == "I":
+            print (dictionary.values)
+        elif i == "J":
+            print (dictionary.values)
+        elif i == "K":
+            print (dictionary.values)
+        elif i == "L":
+            print (dictionary.values)
+        elif i == "M":
+            print (dictionary.values)
